dfc38b969feb0c8d933fbaaee8387e99699a7bb9
doqkhanh <doqkhanh@82627371-9449-0410-b381-fb1e314210de>
2008-09-27 10:02:33 +0000

    Inventory swap item is completed.

--- trunk/Programming/Source/GameEntities/WeaponItem.cs
+++ trunk/Programming/Source/GameEntities/WeaponItem.cs
@@ -9,47 +9,47 @@ using Engine.EntitySystem;
 
 namespace GameEntities
 {
-	/// <summary>
-	/// Defines the <see cref="WeaponItem"/> entity type.
-	/// </summary>
-	public class WeaponItemType : BulletItemType
-	{
-		[FieldSerialize]
-		WeaponType weaponType;
+    /// <summary>
+    /// Defines the <see cref="WeaponItem"/> entity type.
+    /// </summary>
+    public class WeaponItemType : BulletItemType
+    {
+        [FieldSerialize]
+        WeaponType weaponType;
 
-		/// <summary>
-		/// Gets or sets the item weapon type.
-		/// </summary>
-		[Description( "The item weapon type." )]
-		public WeaponType WeaponType
-		{
-			get { return weaponType; }
-			set { weaponType = value; }
-		}
-	}
+        /// <summary>
+        /// Gets or sets the item weapon type.
+        /// </summary>
+        [Description("The item weapon type.")]
+        public WeaponType WeaponType
+        {
+            get { return weaponType; }
+            set { weaponType = value; }
+        }
+    }
 
-	/// <summary>
-	/// Represents a item of the weapon. When the player take this item it 
-	/// takes a specified weapon.
-	/// </summary>
-	public class WeaponItem : BulletItem
-	{
-		WeaponItemType _type = null; public new WeaponItemType Type { get { return _type; } }
+    /// <summary>
+    /// Represents a item of the weapon. When the player take this item it 
+    /// takes a specified weapon.
+    /// </summary>
+    public class WeaponItem : BulletItem
+    {
+        WeaponItemType _type = null; public new WeaponItemType Type { get { return _type; } }
 
-		protected override bool OnTake( Unit unit )
-		{
-			bool take = base.OnTake( unit );
+        protected override bool OnTake(Unit unit)
+        {
+            bool take = base.OnTake(unit);
 
-			if( Type.WeaponType != null )
-			{
-				PlayerCharacter character = unit as PlayerCharacter;
-                if (character != null && character.TakeItem(Type.WeaponType))
-					take = true;
-			}
-			else
-				Log.Warning( "WeaponItem.OnTake: Type.WeaponType == null" );
+            //if( Type.WeaponType != null )
+            //{
+            //    PlayerCharacter character = unit as PlayerCharacter;
+            //    if( character != null && character.TakeWeapon( Type.WeaponType ) )
+            //        take = true;
+            //}
+            //else
+            //    Log.Warning( "WeaponItem.OnTake: Type.WeaponType == null" );
 
-			return take;
-		}
-	}
+            return unit.Inventory.AddItem(this);
+        }
+    }
 }
